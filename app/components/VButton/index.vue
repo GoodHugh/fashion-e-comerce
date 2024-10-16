b8aae8ad4c9c6141753e6eac7abebbe02fe4f2c7
Nazari <nazariadamenko@gmail.com>
2024-10-16 14:16:19 +0300

    [main] - верстка блока elevateYourStyle

---
+++ app/components/VButton/index.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+  interface Props {
+    title: string
+    theme: 'back' | 'white';
+    handleClick?: () => void;
+  }
+
+  const props = withDefaults(defineProps<Props>(), {
+    theme: 'white',
+  });
+
+  const classListTheme = computed(() => {
+    switch (props.theme) {
+      case 'back':
+        return 'v-button--black';
+      case 'white':
+      default:
+        return 'v-button--white';
+    }
+  })
+
+  const classListModifiers = computed(() => [classListTheme.value]);
+</script>
+
+<template>
+  <button class="v-button" :class="classListModifiers" @click="handleClick">
+    <slot />
+  </button>
+</template>
+
+<style lang="scss">
+.v-button {
+  padding: 11px 18px;
+  font-size: 15px;
+  display: inline-block;
+
+  &--white {
+    color: $color-black-1;
+    background-color: $color-white-2;
+  }
+}
+</style>
--- app/pages/main.vue
+++ app/pages/main.vue
@@ -7,4 +7,5 @@ definePageMeta({
 <template>
   <TopBar />
   <Header />
+  <ElevateYourStyle />
 </template>
